e970f12b0b070a8517e6910313464ac5e07baf42
Thomas Willems <twillems@mit.edu>
2016-09-10 17:41:28 -0400

    Removing math functions we're unlikely to use

--- mathops.h
+++ mathops.h
@@ -13,27 +13,15 @@ extern const double LOG_ONE_HALF;
 extern const double TOLERANCE;
 extern const double LOG_E_BASE_10;
 
-void precompute_integer_logs();
-
-double int_log(int val);
-
 double sum(double* begin, double* end);
 
 double sum(std::vector<double>& vals);
-
-int sum (std::vector<bool>& vals);
 
 double log_sum_exp(double* begin, double* end);
 
 double log_sum_exp(double log_v1, double log_v2);
 
-double log_sum_exp(double log_v1, double log_v2, double log_v3);
-
 double log_sum_exp(std::vector<double>& log_vals);
-
-double expected_value(double* log_probs, std::vector<int>& vals); 
-
-double expected_value(std::vector<double>& log_likelihoods, std::vector<int>& vals);
 
 // To accelerate logsumexp, ignore values if they're 1/1000th or less than the maximum value
 const double LOG_THRESH   = log(0.001);
