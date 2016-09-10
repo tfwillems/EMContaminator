54f2382af67bc8bd6e0bbc9428cd69847ac0ea4e
Thomas Willems <twillems@mit.edu>
2016-09-10 17:36:58 -0400

    Importing relevant files from HipSTR repo

---
+++ mathops.cpp
@@ -0,0 +1,85 @@
+#include <algorithm>
+#include <assert.h>
+#include <math.h>
+
+#include "mathops.h"
+
+const double LOG_ONE_HALF  = log(0.5);
+const double TOLERANCE     = 1e-10;
+const double LOG_E_BASE_10 = 0.4342944819;
+
+double INT_LOGS[10000];
+
+void precompute_integer_logs(){
+  INT_LOGS[0] = -1000;
+  for (unsigned int i = 1; i < 10000; i++)
+    INT_LOGS[i] = log(i);
+}
+
+double int_log(int val){ return INT_LOGS[val]; }
+
+double sum(double* begin, double* end){
+  double total = 0.0;
+  for (double* iter = begin; iter != end; iter++)
+    total += *iter;
+  return total;
+}
+
+double sum(std::vector<double>& vals){
+  double total = 0.0;
+  for (auto iter = vals.begin(); iter != vals.end(); iter++)
+    total += *iter;
+  return total;
+}
+
+int sum(std::vector<bool>& vals){
+  int total = 0;
+  for (auto iter = vals.begin(); iter != vals.end(); iter++)
+    total += *iter;
+  return total;
+}
+
+double log_sum_exp(double* begin, double* end){
+  double max_val = *std::max_element(begin, end);
+  double total   = 0.0;
+  for (double* iter = begin; iter != end; iter++)
+    total += exp(*iter - max_val);
+  return max_val + log(total);
+}
+
+double log_sum_exp(double log_v1, double log_v2){
+  if (log_v1 > log_v2)
+    return log_v1 + log(1 + exp(log_v2-log_v1));
+  else
+    return log_v2 + log(1 + exp(log_v1-log_v2));
+}
+
+double log_sum_exp(double log_v1, double log_v2, double log_v3){
+  double max_val = std::max(std::max(log_v1, log_v2), log_v3);
+  return max_val + log(exp(log_v1-max_val) + exp(log_v2-max_val) + exp(log_v3-max_val));
+}
+
+double log_sum_exp(std::vector<double>& log_vals){
+  double max_val = *std::max_element(log_vals.begin(), log_vals.end());
+  double total   = 0;
+  for (auto iter = log_vals.begin(); iter != log_vals.end(); iter++)
+    total += exp(*iter - max_val);
+  return max_val + log(total);
+}
+
+double expected_value(double* log_likelihoods, std::vector<int>& vals){
+  double total_LL = log_sum_exp(log_likelihoods, log_likelihoods+vals.size());
+  double total    = 0.0;
+  for (unsigned int i = 0; i < vals.size(); i++)
+    total += exp(log_likelihoods[i]-total_LL)*vals[i];
+  return total;
+}
+
+double expected_value(std::vector<double>& log_likelihoods, std::vector<int>& vals){
+  assert(log_likelihoods.size() == vals.size());
+  double total_LL = log_sum_exp(log_likelihoods);
+  double total    = 0.0;
+  for (unsigned int i = 0; i < vals.size(); i++)
+    total += exp(log_likelihoods[i]-total_LL)*vals[i];
+  return total;
+}
---
+++ mathops.h
@@ -0,0 +1,80 @@
+#ifndef MATH_OPS_H_
+#define MATH_OPS_H_
+
+#include <math.h>
+#include <cstdint>
+
+#define cast_uint32_t static_cast<uint32_t>
+
+#include <algorithm>
+#include <vector>
+
+extern const double LOG_ONE_HALF;
+extern const double TOLERANCE;
+extern const double LOG_E_BASE_10;
+
+void precompute_integer_logs();
+
+double int_log(int val);
+
+double sum(double* begin, double* end);
+
+double sum(std::vector<double>& vals);
+
+int sum (std::vector<bool>& vals);
+
+double log_sum_exp(double* begin, double* end);
+
+double log_sum_exp(double log_v1, double log_v2);
+
+double log_sum_exp(double log_v1, double log_v2, double log_v3);
+
+double log_sum_exp(std::vector<double>& log_vals);
+
+double expected_value(double* log_probs, std::vector<int>& vals); 
+
+double expected_value(std::vector<double>& log_likelihoods, std::vector<int>& vals);
+
+// To accelerate logsumexp, ignore values if they're 1/1000th or less than the maximum value
+const double LOG_THRESH   = log(0.001);
+
+static inline float fasterlog (float x){
+  union { float f; uint32_t i; } vx = { x };
+  float y = vx.i;
+  y *= 8.2629582881927490e-8f;
+  return y - 87.989971088f;
+}
+
+static inline float fasterpow2 (float p){
+  float clipp = (p < -126) ? -126.0f : p;
+  union { uint32_t i; float f; } v = { cast_uint32_t ( (1 << 23) * (clipp + 126.94269504f) ) };
+  return v.f;
+}
+
+static inline float fasterexp (float p) {
+  return fasterpow2 (1.442695040f * p);
+}
+
+static inline double fast_log_sum_exp(double log_v1, double log_v2){
+  if (log_v1 > log_v2){
+    double diff = log_v2-log_v1;
+    return diff < LOG_THRESH ? log_v1 : log_v1 + fasterlog(1 + fasterexp(diff));
+  }
+  else {
+    double diff = log_v1-log_v2;
+    return diff < LOG_THRESH ? log_v2 : log_v2 + fasterlog(1 + fasterexp(diff));
+  }       
+}
+
+static inline double fast_log_sum_exp(std::vector<double>& log_vals){
+  double max_val = *std::max_element(log_vals.begin(), log_vals.end());
+  double total   = 0;
+  for (auto iter = log_vals.begin(); iter != log_vals.end(); iter++){
+    double diff = *iter - max_val;
+    if (diff > LOG_THRESH)
+      total += fasterexp(diff);
+  }
+  return max_val + fasterlog(total);
+}
+
+#endif
